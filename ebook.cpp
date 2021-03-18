c0e83716b3ccac40ca4b1ad2b1024c82b170dc5a
fantasyhome <76655785+fantasyhome@users.noreply.github.com>
2021-03-18 17:48:11 +0800

    Add files via upload
    添加了画笔功能

--- ebook.cpp
+++ ebook.cpp
@@ -7,25 +7,91 @@ Ebook::Ebook(QWidget *parent)
     , ui(new Ui::Ebook)
 {
     ui->setupUi(this);
+    ui->draw->setWidth(/*widthSpinBox->value()*/1);
+    ui->draw->setColor(Qt::black);
     ui->show->hide();
     ui->localbook->hide();
+    ui->tool_1->hide();ui->tool_2->hide();ui->tool_3->hide();ui->tool_4->hide();ui->tool_5->hide();ui->tool_6->hide();
+    ui->tool_7->hide();ui->tool_8->hide();ui->tool_9->hide();ui->tool_10->hide();ui->tool_11->hide();ui->tool_12->hide();
+    ui->tool_13->hide();ui->tool_14->hide();ui->tool_15->hide();
+    ui->load_b->hide();ui->username->hide();ui->username_l->hide();ui->code->hide();ui->code_l->hide();
 }
 
 Ebook::~Ebook()
 {
     delete ui;
 }
+void Ebook::zero_on()
+{
+    ui->localbook->show();
+}
+void Ebook::zero_off()
+{
+    ui->localbook->hide();
+}
+void Ebook::one_on()
+{
+    ui->show->show();
+    ui->tool_1->show();ui->tool_2->show();ui->tool_3->show();ui->tool_4->show();ui->tool_5->show();ui->tool_6->show();
+    ui->tool_7->show();ui->tool_8->show();ui->tool_9->show();ui->tool_10->show();ui->tool_11->show();ui->tool_12->show();
+    ui->tool_13->show();ui->tool_14->show();ui->tool_15->show();
+}
+void Ebook::one_off()
+{
+    ui->show->hide();
+    ui->tool_1->hide();ui->tool_2->hide();ui->tool_3->hide();ui->tool_4->hide();ui->tool_5->hide();ui->tool_6->hide();
+    ui->tool_7->hide();ui->tool_8->hide();ui->tool_9->hide();ui->tool_10->hide();ui->tool_11->hide();ui->tool_12->hide();
+    ui->tool_13->hide();ui->tool_14->hide();ui->tool_15->hide();
+}
+void Ebook::two_on()
+{
+
+}
+void Ebook::two_off()
+{
+
+}
+void Ebook::three_on()
+{
+
+}
+void Ebook::three_off()
+{
+
+}
+void Ebook::four_on()
+{
+
+}
+void Ebook::four_off()
+{
+
+}
+void Ebook::five_on()
+{
+
+}
+void Ebook::five_off()
+{
+
+}
+void Ebook::six_on()
+{
+    ui->load_b->show();ui->username->show();ui->username_l->show();ui->code->show();ui->code_l->show();
+}
+void Ebook::six_off()
+{
+    ui->load_b->hide();ui->username->hide();ui->username_l->hide();ui->code->hide();ui->code_l->hide();
+}
 void Ebook::display()
 {
     if(mode==0)
     {
-        ui->show->hide();
-        ui->localbook->show();
+        zero_on();one_off();two_off();three_off();four_off();five_off();six_off();
     }
     if(mode==1)
     {
-        ui->show->show();
-        ui->localbook->hide();
+        zero_off();one_on();two_off();three_off();four_off();five_off();six_off();
         QFile file("test.txt");
         if(!file.open(QIODevice::ReadOnly | QIODevice::Text))
         {
@@ -38,6 +104,10 @@ void Ebook::display()
             ui->show->textCursor().insertText(str);
             ui->show->moveCursor(QTextCursor::End);
         }
+    }
+    if(mode==6)
+    {
+        zero_off();one_off();two_off();three_off();four_off();five_off();six_on();
     }
 }
 
@@ -75,4 +145,27 @@ void Ebook::on_system_clicked()
 {
     mode=5;
     display();
+}
+
+void Ebook::on_localbook_clicked()
+{
+    mode=1;
+    display();
+}
+
+void Ebook::on_load_clicked()
+{
+    mode=6;
+    display();
+}
+
+
+void Ebook::on_load_b_clicked()
+{
+
+}
+
+void Ebook::on_draw_customContextMenuRequested(const QPoint &pos)
+{
+
 }
