838f8b2513b6e1b9566025167d0adb74489af4b3
fantasyhome <76655785+fantasyhome@users.noreply.github.com>
2021-03-23 00:15:18 +0800

    把工具栏整合进了按钮，实现了多样化画笔

--- DrawWidget.cpp
+++ DrawWidget.cpp
@@ -1,13 +1,14 @@
 #include"DrawWidget.h"
 #include <QtGui>
 #include <QPen>
+
+extern bool paint_on;
 DrawWidget::DrawWidget(QWidget *parent) : QWidget(parent)
 {
     setAutoFillBackground(true);    	//对窗体背景色的设置
-    setPalette(QPalette(Qt::white));
+    setPalette(QPalette(Qt::transparent));
     pix =new QPixmap(size());           //此QPixmap对象用来准备随时接收绘制的内容
-    pix->fill(Qt::white);            	//填充背景色为白色
- //   setMinimumSize(600,400);            //设置绘制区窗体的最小尺寸
+    pix->fill(Qt::transparent);            	//填充背景色为透明
 }
 void DrawWidget::setStyle(int s)
 {
@@ -27,11 +28,13 @@ void DrawWidget::mousePressEvent(QMouseEvent *e)
 }
 void DrawWidget::mouseMoveEvent(QMouseEvent *e)
 {
+    if(paint_on==1)
+    {
     QPainter *painter = new QPainter;		//新建一个QPainter对象
     QPen pen;								//新建一个QPen对象
-    pen.setStyle((Qt::PenStyle)1/*style*/);		//(a)
-    pen.setWidth(/*weight*/1);					//设置画笔的线宽值
-    pen.setColor(/*color*/Qt::black);					//设置画笔的颜色
+    pen.setStyle((Qt::PenStyle)style);		//(a)
+    pen.setWidth(weight);					//设置画笔的线宽值
+    pen.setColor(color);					//设置画笔的颜色
     painter->begin(pix);					//(b)
     painter->setPen(pen);					//将QPen对象应用到绘制对象中
     //绘制从startPos到鼠标当前位置的直线
@@ -39,6 +42,7 @@ void DrawWidget::mouseMoveEvent(QMouseEvent *e)
     painter->end();
     startPos =e->pos();				//更新鼠标的当前位置，为下次绘制做准备
     update();						//重绘绘制区窗体
+    }
 }
 void DrawWidget::paintEvent(QPaintEvent *)
 {
@@ -50,7 +54,7 @@ void DrawWidget::resizeEvent(QResizeEvent *event)
     if(height()>pix->height()||width()>pix->width())	//(a)
     {
         QPixmap *newPix = new QPixmap(size());	//创建一个新的QPixmap对象
-        newPix->fill(Qt::white);                //填充新QPixmap对象newPix的颜色为白色背景色
+        newPix->fill(Qt::transparent);                //填充新QPixmap对象newPix的颜色为透明背景色
         QPainter p(newPix);
         p.drawPixmap(QPoint(0,0),*pix);         //在newPix中绘制原pix中的内容
         pix = newPix;                           //将newPix赋值给pix作为新的绘制图形接收对象
@@ -60,7 +64,7 @@ void DrawWidget::resizeEvent(QResizeEvent *event)
 void DrawWidget::clear()
 {
     QPixmap *clearPix =new QPixmap(size());
-    clearPix->fill(Qt::white);
+    clearPix->fill(Qt::transparent);
     pix = clearPix;
     update();
 }
--- ebook.cpp
+++ ebook.cpp
@@ -1,25 +1,67 @@
 #include "ebook.h"
+#include"DrawWidget.h"
 #include "ui_ebook.h"
 #include <QFile>
 int mode=0;
+bool paint_on=0;
 Ebook::Ebook(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Ebook)
 {
     ui->setupUi(this);
-    ui->draw->setWidth(/*widthSpinBox->value()*/1);
+    ShowStyle();
+    ui->draw->setWidth(ui->widthSpinBox->value());
     ui->draw->setColor(Qt::black);
+    ui->draw->setStyle(Qt::SolidLine);
     ui->show->hide();
     ui->localbook->hide();
-    ui->tool_1->hide();ui->tool_2->hide();ui->tool_3->hide();ui->tool_4->hide();ui->tool_5->hide();ui->tool_6->hide();
-    ui->tool_7->hide();ui->tool_8->hide();ui->tool_9->hide();ui->tool_10->hide();ui->tool_11->hide();ui->tool_12->hide();
-    ui->tool_13->hide();ui->tool_14->hide();ui->tool_15->hide();
+    tool_off();
     ui->load_b->hide();ui->username->hide();ui->username_l->hide();ui->code->hide();ui->code_l->hide();
+    createToolBar();
+    ToolBar_off();
 }
-
+void Ebook::ShowStyle()
+{
+    ui->draw->setStyle(ui->styleComboBox->itemData(
+        ui->styleComboBox->currentIndex(),Qt::UserRole).toInt());
+}
+void Ebook::ShowColor()
+{
+    QColor color = QColorDialog::getColor(static_cast<int> (Qt::black), this);
+    //使用标准颜色对话框QColorDialog获得一个颜色值
+    if(color.isValid())
+    {
+       //将新选择的颜色传给绘制区，用于改变画笔的颜色值
+        ui->draw->setColor(color);
+        QPixmap p(20,20);
+        p.fill(color);
+        ui->colorBtn->setIcon(QIcon(p));		//更新颜色选择按钮上的颜色显示
+    }
+}
 Ebook::~Ebook()
 {
     delete ui;
+}
+void Ebook::tool_on()
+{
+
+    ui->tool_1->show();ui->tool_2->show();ui->tool_3->show();ui->tool_4->show();ui->tool_5->show();ui->tool_6->show();
+    ui->tool_7->show();ui->tool_8->show();ui->tool_9->show();ui->tool_10->show();ui->tool_11->show();ui->tool_12->show();
+    ui->tool_13->show();ui->tool_14->show();ui->tool_15->show();
+}
+void Ebook::tool_off()
+{
+    ui->tool_1->hide();ui->tool_2->hide();ui->tool_3->hide();ui->tool_4->hide();ui->tool_5->hide();ui->tool_6->hide();
+    ui->tool_7->hide();ui->tool_8->hide();ui->tool_9->hide();ui->tool_10->hide();ui->tool_11->hide();ui->tool_12->hide();
+    ui->tool_13->hide();ui->tool_14->hide();ui->tool_15->hide();
+}
+void Ebook::ToolBar_on()
+{
+    ui->label->show();ui->label_2->show();ui->styleComboBox->show();ui->widthSpinBox->show();ui->colorBtn->show();ui->clearBtn->show();
+}
+void Ebook::ToolBar_off()
+{
+    ui->label->hide();ui->label_2->hide();ui->styleComboBox->hide();ui->widthSpinBox->hide();ui->colorBtn->hide();ui->clearBtn->hide();
 }
 void Ebook::zero_on()
 {
@@ -32,16 +74,14 @@ void Ebook::zero_off()
 void Ebook::one_on()
 {
     ui->show->show();
-    ui->tool_1->show();ui->tool_2->show();ui->tool_3->show();ui->tool_4->show();ui->tool_5->show();ui->tool_6->show();
-    ui->tool_7->show();ui->tool_8->show();ui->tool_9->show();ui->tool_10->show();ui->tool_11->show();ui->tool_12->show();
-    ui->tool_13->show();ui->tool_14->show();ui->tool_15->show();
+    ui->draw->show();
+    tool_on();
 }
 void Ebook::one_off()
 {
     ui->show->hide();
-    ui->tool_1->hide();ui->tool_2->hide();ui->tool_3->hide();ui->tool_4->hide();ui->tool_5->hide();ui->tool_6->hide();
-    ui->tool_7->hide();ui->tool_8->hide();ui->tool_9->hide();ui->tool_10->hide();ui->tool_11->hide();ui->tool_12->hide();
-    ui->tool_13->hide();ui->tool_14->hide();ui->tool_15->hide();
+    ui->draw->hide();
+    tool_off();
 }
 void Ebook::two_on()
 {
@@ -104,6 +144,17 @@ void Ebook::display()
             ui->show->textCursor().insertText(str);
             ui->show->moveCursor(QTextCursor::End);
         }
+        if(paint_on)
+        {
+            ToolBar_on();
+            tool_off();
+            ui->tool_11->show();
+        }
+        else
+        {
+            ToolBar_off();
+            tool_on();
+        }
     }
     if(mode==6)
     {
@@ -158,7 +209,23 @@ void Ebook::on_load_clicked()
     mode=6;
     display();
 }
+void Ebook::createToolBar()
+{
+    ui->styleComboBox->addItem(tr("SolidLine"),
+                                static_cast<int>(Qt::SolidLine));
+    ui->styleComboBox->addItem(tr("DashLine"),
+                                static_cast<int>(Qt::DashLine));
+    ui->styleComboBox->addItem(tr("DotLine"),
+                                static_cast<int>(Qt::DotLine));
+    ui->styleComboBox->addItem(tr("DashDotLine"),
+                                static_cast<int>(Qt::DashDotLine));
+    ui->styleComboBox->addItem(tr("DashDotDotLine"),
+                                static_cast<int>(Qt::DashDotDotLine));
 
+    QPixmap pixmap(20,20);
+    pixmap.fill(Qt::black);
+    ui->colorBtn->setIcon(QIcon(pixmap));
+}
 
 void Ebook::on_load_b_clicked()
 {
@@ -168,4 +235,34 @@ void Ebook::on_load_b_clicked()
 void Ebook::on_draw_customContextMenuRequested(const QPoint &pos)
 {
 
+}
+
+void Ebook::on_tool_11_clicked()
+{
+    paint_on=!paint_on;
+    display();
+}
+
+void Ebook::on_styleComboBox_activated(int index)
+{
+    ShowStyle();
+    display();
+}
+
+void Ebook::on_widthSpinBox_valueChanged(int arg1)
+{
+    ui->draw->setWidth(arg1);
+    display();
+}
+
+void Ebook::on_colorBtn_clicked()
+{
+    ShowColor();
+    display();
+}
+
+void Ebook::on_clearBtn_clicked()
+{
+    ui->draw->clear();
+    display();
 }
